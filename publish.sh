cbfe552e1e6e8d35f4d514191aca771bcfb26b8d
cristi0204 <67113543+cristi0204@users.noreply.github.com>
2023-05-12 18:25:59 +0300

    helm dep up

--- publish.sh
+++ publish.sh
@@ -50,7 +50,7 @@ git clone -b "$GITHUB_PAGES_BRANCH" "git@github.com:$GITHUB_PAGES_REPO.git" .
 echo '>> Building charts...'
 find "$HELM_CHARTS_SOURCE" -mindepth 1 -maxdepth 1 -type d | while read chart; do
   echo ">>>> helm dependency update"
-  helm dep up
+  helm dep up "$chart"
   echo ">>> helm lint $chart"
   helm lint "$chart"
   chart_name="`basename "$chart"`"
