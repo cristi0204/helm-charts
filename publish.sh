3da45c42d66b192ab0aea8bce5c8053594145a28
cristi0204 <67113543+cristi0204@users.noreply.github.com>
2023-05-12 14:53:38 +0300

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -34,10 +34,11 @@ apk add ca-certificates git openssh
 
 echo '>> Installing Helm...'
 cd /tmp/helm/bin
-wget "https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz"
-tar -zxf "helm-v${HELM_VERSION}-linux-amd64.tar.gz"
-chmod +x linux-amd64/helm
-alias helm=/tmp/helm/bin/linux-amd64/helm
+#wget "https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz"
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+#tar -zxf "helm-v${HELM_VERSION}-linux-amd64.tar.gz"
+#chmod +x linux-amd64/helm
+#alias helm=/tmp/helm/bin/linux-amd64/helm
 helm version -c
 helm init -c
 
